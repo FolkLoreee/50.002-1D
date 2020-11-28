c322efca6eee6a260129cd247036e498ad0ecd4e
Filbert Cia <cia.filbert@gmail.com>
2020-11-28 11:48:39 +0800

    constraint issue

--- work/project.tcl
+++ work/project.tcl
@@ -7,7 +7,7 @@ create_project $projName "$projDir/$projName" -part $device
 set_property design_mode RTL [get_filesets sources_1]
 set verilogSources [list "/home/folkloreee/Documents/blinding_lights/work/verilog/au_top_0.v" "/home/folkloreee/Documents/blinding_lights/work/verilog/reset_conditioner_1.v" ]
 import_files -fileset [get_filesets sources_1] -force -norecurse $verilogSources
-set xdcSources [list "/home/folkloreee/Documents/alchitry-labs-1.2.1/library/components/au.xdc" "/home/folkloreee/Documents/blinding_lights/constraint/blinding_lights.xdc" "/home/folkloreee/Documents/blinding_lights/work/constraint/custom.xdc" ]
+set xdcSources [list "/home/folkloreee/Documents/blinding_lights/constraint/blinding_lights.xdc" "/home/folkloreee/Documents/blinding_lights/work/constraint/custom.xdc" ]
 read_xdc $xdcSources
 set_property STEPS.WRITE_BITSTREAM.ARGS.BIN_FILE true [get_runs impl_1]
 update_compile_order -fileset sources_1
